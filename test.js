37dc11d2dedf1ad0671a9b273332a50241237686
Junior Lourenço <juniorminaj101@gmail.com>
2020-08-24 03:02:49 -0400

    update

---
+++ test.js
@@ -0,0 +1,10 @@
+const crypto = require("crypto");
+const start = Date.now();
+
+function logHashtime(){
+    crypto.pbkdf2("a","b", 100000,512, "sha512", () =>{
+        console.log(`Hash: ${Date.now() - start}`);
+    })
+}
+
+logHashtime();

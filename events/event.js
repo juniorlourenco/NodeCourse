282bfe5b9a18c8912f2dd14e7aa5436e960812e3
Junior Lourenço <juniorminaj101@gmail.com>
2020-08-26 00:35:08 -0400

    adding events

---
+++ events/event.js
@@ -0,0 +1,11 @@
+const EventEmitter = require('events');
+
+class Evento extends EventEmitter {}
+
+const meuEvento = new Evento()
+
+meuEvento.on('seguranca', (x,y)=>{
+    console.log(`Executando o evento 'segurança': ${x} ${y}`)
+})
+
+meuEvento.emit('seguranca', 'userAdmin', 'Alterou Salário')

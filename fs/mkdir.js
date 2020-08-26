947dbf075b49b1f3a718c703cf07264a0c63e047
Junior Lourenço <juniorminaj101@gmail.com>
2020-08-26 03:38:19 -0400

    filesystem creating directories

---
+++ fs/mkdir.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const assets = ['css', 'js', 'images', 'fonts', 'lib']
+
+function make(dir){
+
+    dir.forEach((item) => {
+        fs.mkdir(`projeto/assets/${item}`,{recursive: true}, (err)=>{
+            if(err) throw err;
+            console.log('Diretório criado com sucesso: ', item)
+        })
+    });
+}
+make(assets);
+

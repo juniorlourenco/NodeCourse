64806f1fe5e6d0833654df83c5a1e71f681edf0c
Junior Lourenço <juniorminaj101@gmail.com>
2020-08-26 00:58:23 -0400

    working with process.env

---
+++ tools/process.js
@@ -0,0 +1,25 @@
+console.log(`Nome do arquivo: `,__filename)
+console.log(`Nome do diretório: `,__dirname)
+console.log(`Parâmetros de execução: `,process.argv)
+console.log(`Diretório em que foi invocado`, process.cwd())
+console.log(`Sistema Operacional:`, process.env.OS)
+console.log(`Nome do usuário no Sistema operacional:`, process.env.USERNAME)
+console.log(`Idioma:`, process.env.LANG)
+console.log(`Nome do server:`, process.env.COMPUTERNAME)
+
+switch(process.argv[2]){
+    
+    case '-a' :
+        console.log('Execute a rotina principal');
+        break;
+    case '-i' :
+        console.log('Execute instalação');
+        break;
+    case '-q' :
+        console.log('Encerrando a aplicação');
+        process.exit() // interrompe imediatamente o código
+        break;
+    default :
+        console.log('Parâmetro inválido');        
+}
+console.log(`Ambiente do servidor: `,process.platform)

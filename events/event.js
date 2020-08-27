becab3ec39fc3b709247c4c501224cb17e57b5e8
Junior Lourenço <juniorminaj101@gmail.com>
2020-08-27 03:44:52 -0400

    event on and event emit

--- events/event.js
+++ events/event.js
@@ -1,11 +1,19 @@
-const EventEmitter = require('events');
+const { EventEmitter } = require('events');
 
 class Evento extends EventEmitter {}
 
 const meuEvento = new Evento()
 
+//subscriber
 meuEvento.on('seguranca', (x,y)=>{
     console.log(`Executando o evento 'segurança': ${x} ${y}`)
 })
 
+//publisher
 meuEvento.emit('seguranca', 'userAdmin', 'Alterou Salário')
+
+meuEvento.on('encerrar', (dados)=>{
+    console.log('Assinante: ' + dados);
+});
+
+meuEvento.emit('encerrar', 'Encerrando a execução da importação dos dados!');

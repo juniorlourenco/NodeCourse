1fd21253b1262946c69a13590cb5a97269ac04a9
Junior Lourenço <juniorminaj101@gmail.com>
2020-08-28 02:15:07 -0400

    creating and exporting modules

---
+++ modules/module-01.js
@@ -0,0 +1,11 @@
+console.log('Executando module-01.js');
+
+module.exports.oculta = () => {
+    console.log('Executando função oculta');
+};
+
+module.exports.executa = () => {
+    console.log('Executando a função executa');
+};
+
+module.exports.welcome = 'Bem vindo ao módulo module-01.js';

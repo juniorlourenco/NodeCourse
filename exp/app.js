b400ce8b8ed6a85fad740b9966d39599b0640a3b
Junior Lourenço <juniorminaj101@gmail.com>
2020-08-28 06:55:52 -0400

    adding admin route by id

--- exp/app.js
+++ exp/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.statusCode = 200;
     res.send('Hello World');
+});
+
+app.get('/admin', (req,res) => {
+    res.send('Acessando a Administração');
+});
+
+app.get('/admin/:id', (req,res) => {
+    res.send('Acessando a Administração com o id:'+req.params.id);
 });
 
 app.listen(3000, () => {

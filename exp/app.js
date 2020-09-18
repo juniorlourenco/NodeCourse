6b18ca84706b14e21938d7f39bc7f5f66ea145a9
Junior Lourenço <juniorminaj101@gmail.com>
2020-09-18 11:35:58 -0400

    Cookie Parser and Middleware functions handling errors

--- exp/app.js
+++ exp/app.js
@@ -3,15 +3,30 @@ const express = require('express');
 const app = express();
 const adminRoutes = require('./routes/admin');
 const userRoutes = require('./routes/user');
+const cookieParser = require('cookie-parser');
 
 app.use('/static',express.static('public'));
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.use((req, res, next) =>{
     console.log('Executando Middleware em nível de aplicação')
 
     return next();
+});
+
+app.get('/setcookie', (req, res)=>{
+    res.cookie('user', 'Junior Lourenço', {maxAge: 900000, httpOnly: true})
+    return res.send('Cookie foi gravado com sucesso!');
+});
+
+app.get('/getcookie', (req, res)=>{
+    let user = req.cookies['user'];
+    if(user){
+        return res.send(user);
+    };
 });
 
 app.get('/',(req, res)=>{
@@ -27,9 +42,15 @@ app.get('*',(req, res, next)=>{
     })
 })
 
-app.use((err, req, res, next)=>{
-    console.log('Geramos um erro, veja as instruções para corrigir!');
+app.use((err,req, res, next)=>{
+    console.log(err.stack);
     res.status(500).json({message: err.message});
+})
+
+app.get('*',(req, res, next)=>{
+    setImmediate(()=>{
+        next( new Error('Temos um problema'));
+    })
 })
 
 app.listen(3000, ()=>{

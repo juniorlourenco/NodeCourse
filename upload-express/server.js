ebbd3afdde086a223497a70a0a17737b0053a35a
juniorlourenco <56567317+juniorlourenco@users.noreply.github.com>
2020-09-20 22:56:10 -0400

    Html form

--- upload-express/server.js
+++ upload-express/server.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
-    res.json({message: 'Bem vindo'});
+    //res.json({message: 'Bem vindo'});
+    res.sendFile(__dirname+'/index.html')
 });
 
 app.listen(3000, '127.0.0.1', () => {

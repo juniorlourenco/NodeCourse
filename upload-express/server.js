914695b94cb00bc2f86ec060782c8760e84ee0f8
juniorlourenco <56567317+juniorlourenco@users.noreply.github.com>
2020-09-20 23:14:59 -0400

    Setting up multer storage

--- upload-express/server.js
+++ upload-express/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const app = express();
+const path = require('path');
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -9,6 +10,17 @@ app.get('/', (req, res) => {
     //res.json({message: 'Bem vindo'});
     res.sendFile(__dirname+'/index.html')
 });
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname+'-'+Date.now()+path.extname(file.originalname));
+    }
+});
+
+const upload = multer({storage});
 
 app.listen(3000, '127.0.0.1', () => {
     console.log(`Server running on port 3000`);

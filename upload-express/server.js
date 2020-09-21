5f0bd8c275d529d9ebf4b82d8aa0af1d103d2783
juniorlourenco <56567317+juniorlourenco@users.noreply.github.com>
2020-09-20 23:34:13 -0400

    Setting up POST method

--- upload-express/server.js
+++ upload-express/server.js
@@ -6,11 +6,6 @@ const path = require('path');
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/', (req, res) => {
-    //res.json({message: 'Bem vindo'});
-    res.sendFile(__dirname+'/index.html')
-});
-
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads/');
@@ -21,6 +16,21 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({storage});
+
+app.get('/', (req, res) => {
+    //res.json({message: 'Bem vindo'});
+    res.sendFile(__dirname+'/index.html')
+});
+
+app.post('/upload', upload.single('arquivo'), (req, res, next) => {
+    const file = req.file
+    if(!file){
+        const err = new Error('Por favor selecione um arquivo');
+        err.httpStatusCode = 400;
+        return next(err);
+    }
+    res.send(file);
+});
 
 app.listen(3000, '127.0.0.1', () => {
     console.log(`Server running on port 3000`);

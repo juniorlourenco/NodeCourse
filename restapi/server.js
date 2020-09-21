9bb324abc9b2653cfdfda345d3d42c8408a5c154
Junior Lourenço <juniorminaj101@gmail.com>
2020-09-21 11:31:06 -0400

    Creating initial routes

--- restapi/server.js
+++ restapi/server.js
@@ -10,6 +10,9 @@ db.once('open', () => console.log('Database connected'));
 
 app.use(express.json());
 
+const subscribersRouter = require('./routes/subscribers');
+app.use('/subscribers', subscribersRouter);
+
 app.listen(3000, () =>
     console.log('Server running!')
 );

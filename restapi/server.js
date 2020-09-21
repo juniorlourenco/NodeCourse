446e55a15e18cf434d4ab05981457726f7f188e4
Junior Lourenço <juniorminaj101@gmail.com>
2020-09-21 11:23:47 -0400

    Creating environment variables

--- restapi/server.js
+++ restapi/server.js
@@ -1,11 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/subscribers", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DATABASE_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection
 db.on('error', (err) => console.log(err));
 db.once('open', () => console.log('Database connected'));
+
+app.use(express.json());
 
 app.listen(3000, () =>
     console.log('Server running!')
